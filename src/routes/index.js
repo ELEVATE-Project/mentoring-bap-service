41dead46e776cb13d598177a3b2254574b275224
joffinjoy <joffinjoy@gmail.com>
2022-09-30 15:09:39 +0530

    Added Routes For New APIs

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,4 +5,10 @@ const bap = require('@controllers/')
 router.get('/search', bap.search)
 router.post('/on_search', bap.onSearch)
 
+router.post('/init', bap.init)
+router.post('/on_init', bap.onInit)
+
+router.post('/confirm', bap.confirm)
+router.post('/on_confirm', bap.onConfirm)
+
 module.exports = router
