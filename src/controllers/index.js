a7d25b3499f7da9b00cd78b14d05b10f6cf137e9
joffinjoy <joffinjoy@gmail.com>
2022-10-25 15:04:42 +0530

    Removed Minimum Wait Time From Status  API

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -184,11 +184,12 @@ exports.status = async (req, res) => {
 			requestBodyGenerator('bpp_status', { orderId }, transactionId, messageId),
 			{ shouldSign: true }
 		)
-		setTimeout(async () => {
-			const data = await cacheGet(`${transactionId}:${messageId}:ON_STATUS`)
-			if (!data) res.status(403).send({ message: 'No data Found' })
-			else res.status(200).send({ data: data })
-		}, 1000)
+		const message = await getMessage(`${transactionId}:${messageId}`)
+		if (message !== transactionId + messageId)
+			return res.status(400).json({ message: 'Something Went Wrong (Redis Message Issue)' })
+		const data = await cacheGet(`${transactionId}:${messageId}:ON_STATUS`)
+		if (!data) return res.status(403).send({ message: 'No data Found' })
+		res.status(200).send({ data: data })
 	} catch (err) {
 		console.log(err)
 		res.status(400).send({ status: false })
@@ -200,6 +201,7 @@ exports.onStatus = async (req, res) => {
 		const transactionId = req.body.context.transaction_id
 		const messageId = req.body.context.message_id
 		await cacheSave(`${transactionId}:${messageId}:ON_STATUS`, req.body)
+		await sendMessage(`${transactionId}:${messageId}`, transactionId + messageId)
 		res.status(200).json({ status: true, message: 'BAP Received STATUS From BPP' })
 	} catch (err) {}
 }
